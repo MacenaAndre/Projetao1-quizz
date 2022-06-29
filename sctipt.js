6dc54fffd4c37d7ea23e36961ef6d1d2eddb574b
MacenaAndre <andremacenacosta@gmail.com>
2022-06-29 16:54:44 -0300

    página 1 completa

--- sctipt.js
+++ sctipt.js
@@ -8,3 +8,34 @@ let paginaTresUm = ` <h3>Comece pelo começo</h3>
 <div class="botao-informacoes">
     <span>Prosseguir pra criar perguntas</span> 
 </div>`;
+
+let listaquizz;
+
+function carregarPublicos() {
+
+    const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+
+    promise.catch(carregouErro);
+    promise.then(carregouSucesso);
+}
+function carregouErro (Erro) {
+     alert("deu ruim");
+}
+function carregouSucesso (resposta) {
+    console.log(resposta.data)
+    listaquizz = resposta.data;
+    const elemento = document.querySelector(".quizzes");
+
+    elemento.innerHTML = "";
+
+    for(let i = listaquizz.length - 6; i < listaquizz.length; i++) {
+        elemento.innerHTML += `
+        <div class="quizz">
+            <img class="img-quizz" src="${listaquizz[i].image}" alt="">
+           <p class="titulo-quizz">${listaquizz[i].title}</p>
+        </div>
+        `
+    }
+    
+}
+carregarPublicos();
