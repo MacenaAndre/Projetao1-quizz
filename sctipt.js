bafa8b1bf325092a4b1260537bc554e936f12725
Bismarck <bismarckdados@gmail.com>
2022-06-29 15:03:01 -0300

    Guardando pagina 3.1

---
+++ sctipt.js
@@ -0,0 +1,10 @@
+let paginaTresUm = ` <h3>Comece pelo começo</h3>
+<div class="campo informacoes">
+    <input type="text" placeholder="Título do seu quizz">
+    <input type="text" placeholder="URL da imagem do seu quizz">
+    <input type="text" placeholder="Quantidade de perguntas do quizz">
+    <input type="text" placeholder="Quantidade de níveis do quizz">
+</div>
+<div class="botao-informacoes">
+    <span>Prosseguir pra criar perguntas</span> 
+</div>`;
