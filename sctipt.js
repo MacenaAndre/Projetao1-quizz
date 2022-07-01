89c805e656844372f75699b640228caeed533eee
MacenaAndre <andremacenacosta@gmail.com>
2022-07-01 15:54:07 -0300

    Pagina 3.3

--- sctipt.js
+++ sctipt.js
@@ -69,14 +69,66 @@ function renderizarPaginaTresUm(){
             <input class="perguntas-quizz" type="text" placeholder="Quantidade de perguntas do quizz">
             <input class="niveis-quizz" type="text" placeholder="Quantidade de níveis do quizz">
         </div>
-        <div class="botao-informacoes" onclick="avancaPerguntas()">
+        <div class="botao-informacoes" onclick="verificarInformacoesBasicas()">
             Prosseguir pra criar perguntas
         </div>
     </div>
     `
 }
+function verificarInformacoesBasicas(){
+    const titulo = document.querySelector(".titulo").value;
+    const url = document.querySelector(".url-quizz").value;
+    const perguntas = document.querySelector(".perguntas-quizz").value;
+    const niveis = document.querySelector(".niveis-quizz").value;
+
+    if(titulo.length < 20 || titulo > 65){
+        alert('O título precisa ter entre 20 e 65 caracteres.');
+        return false;
+    }
+    if(!url.includes('https://')){
+        alert('Formato de imagem inválido, digite imagens em URL');
+        return false;
+    }
+    if(niveis < 2){
+        alert('O número mínimo de níveis é 2.');
+        return false;
+    }
+    if(perguntas < 3){
+        alert('O número mínimo de perguntas é 3.');
+        return false;
+    }
+    
+    objetoPost.title = titulo;
+    objetoPost.image = url;
+    numNiveis = Number(niveis);
+    numPerguntas = Number(perguntas);
+    objetoNiveis();
+    objetoPerguntas();
+    renderizarPaginaTresDois();
+}
+function objetoNiveis() {
+    for(let i = 0; i < numNiveis; i++) {
+        objetoPost.levels.push({
+            title: "",
+            image: "",
+            text: "",
+            minValue: 0
+        })
+    }
+    console.log(objetoPost.levels);
+}
+function objetoPerguntas() {
+    for(let i = 0; i < numPerguntas; i++) {
+        objetoPost.questions.push({
+            title: "",
+            color: "",
+            answers: []
+        })
+    }
+    console.log(objetoPost.questions);
+}
 function renderizarPaginaTresDois(){
-    document.querySelector('.conteudo').innerHTML += `<h3>Crie suas perguntas</h3>`
+    document.querySelector('.conteudo').innerHTML = `<h3>Crie suas perguntas</h3>`
     for(let i = 0; i < numPerguntas;i++){
         document.querySelector('.conteudo').innerHTML += 
             `<div class="page3-2">
@@ -125,7 +177,7 @@ function renderizarPaginaTresDois(){
         `
     }
     document.querySelector('.conteudo').innerHTML += 
-        `<div class="botao-informacoes" onclick="avancaFim()">
+        `<div class="botao-informacoes" onclick="avancaNiveis()">
               Prosseguir pra criar níveis
         </div>  
         `;
@@ -134,45 +186,6 @@ function renderizarPaginaTresDois(){
         </div>  
         `*/
     
-}
-function avancaPerguntas(){
-    if(verificarInformacoesBasicas()){
-        document.querySelector('.conteudo').innerHTML = ''
-        renderizarPaginaTresDois();
-    }  
-    console.log(objetoPost);
-}
-function verificarInformacoesBasicas(){
-    const titulo = document.querySelector(".titulo").value;
-    const url = document.querySelector(".url-quizz").value;
-    const perguntas = document.querySelector(".perguntas-quizz").value;
-    const niveis = document.querySelector(".niveis-quizz").value;
-
-    if(titulo.length < 20 || titulo > 65){
-        alert('O título precisa ter entre 20 e 65 caracteres.');
-        return false;
-    }
-    if(!url.includes('https://')){
-        alert('Formato de imagem inválido, digite imagens em URL');
-        return false;
-    }
-    if(niveis < 2){
-        alert('O número mínimo de níveis é 2.');
-        return false;
-    }
-    if(perguntas < 3){
-        alert('O número mínimo de perguntas é 3.');
-        return false;
-    }
-    
-    objetoPost.title = titulo;
-    objetoPost.image = url;
-    numNiveis = Number(niveis);
-    numPerguntas = Number(perguntas);
-    objetoNiveis(numNiveis);
-    objetoPerguntas(numPerguntas);
-
-    return true;
 }
 function verificarPerguntas(){
     for(let i = 0; i < numPerguntas; i++){
@@ -213,7 +226,7 @@ function verificarPerguntas(){
                 alert(`Caixa ${i+1}: Resposta Incorreta 2 não pode estar vazia.`);
                 return
             }
-            if(urlIncorreta2.includes('https://')){
+            if(!urlIncorreta2.includes('https://')){
                 alert(`Caixa ${i+1}: Formato da URL Incorreta 2 está inválido, digite imagens em URL válidas`);
                 return;
             }
@@ -223,45 +236,22 @@ function verificarPerguntas(){
                 alert(`Caixa ${i+1}: Resposta Incorreta 3 não pode estar vazia.`);
                 return
             }
-            if(urlIncorreta3.includes('https://')){
+            if(!urlIncorreta3.includes('https://')){
                 alert(`Caixa ${i+1}: Formato da URL Incorreta 3 está inválido, digite imagens em URL válidas`);
                 return;
             }
         }
     }
 }
-
-function objetoNiveis(numero) {
-    for(let i = 0; i < numNiveis; i++) {
-        objetoPost.levels.push({
-            title: "",
-            image: "",
-            text: "",
-            minValeu: 0
-        })
-    }
-    console.log(objetoPost.levels);
-}
-function objetoPerguntas(numero) {
-    for(let i = 0; i < numPerguntas; i++) {
-        objetoPost.questions.push({
-            title: "",
-            color: "",
-            answers: []
-        })
-    }
-    console.log(objetoPost.questions);
-}
 function renderizarPaginaTresTres() {
     document.querySelector('.conteudo').innerHTML = `<h3>Agora, decida os níveis!</h3>`
     for(let i = 0; i < numNiveis; i++) {
     document.querySelector(".conteudo").innerHTML += `
         <div class="page3-3">
-            
-                <div class="campo">
+                <div class="campo nivel${i}">
                     <div class="campo-topo" onclick="selecionarPergunta(this)">
-                        <div class="campo-niveis nivel${i}">
                         <h4>Nível ${i + 1}</h4>
+                        <img  src="./images/Vector.png" alt="">
                     </div>
                     <div class="campo-corpo escondido">
                         <input class="titulo-nivel" type="text" placeholder="Título do nível">
@@ -269,51 +259,80 @@ function renderizarPaginaTresTres() {
                         <input class="url-nivel" type="text" placeholder="URL da imagem do nível">
                         <input class="descricao-nivel" type="text" placeholder="Descrição do nível">
                     </div>
-                </div>    
-        </div>
+                </div> 
+        </div>   
     `;
     }
     document.querySelector(".conteudo").innerHTML +=  `
-       <div class="botao-informacoes" onclick="">
+       <div class="botao-informacoes" onclick="verficiarNiveis()">
            Finalizar Quizz
         </div>
         `  
 }
-function avancaFim() {
-        renderizarPaginaTresTres()
-}
-function verficiarNiveis () {
+function verficiarNiveis() {
+
+    let contador = 0;
 
     for(i = 0; i < numNiveis; i++) {
         let niveis = document.querySelector(`.nivel${i}`);
-        const titulo = niveis.querySelector(".nivel.titulo-nivel").value;
+        const titulo = niveis.querySelector(".titulo-nivel").value;
         const url = niveis.querySelector(".url-nivel").value;
-        const porcentagem = niveis.querySelector(".pctg-nivel").value;
+        const porcentagem = (niveis.querySelector(".pctg-nivel").value);
         const descricao = niveis.querySelector(".descricao-nivel").value;
 
-    if(titulo.length < 10)  {
-        alert('O título precisa ter pelo menos 10 caractéres.');
+        if(titulo.length < 10)  {
+           alert(`O título precisa ter pelo menos 10 caractéres em Nivel ${i + 1}.`);
+           return false;
+        }
+        if(porcentagem === "" || isNaN(Number(porcentagem)) || Number(porcentagem) < 0 || Number(porcentagem) > 100) {
+            alert(`Porcentagem inválida Nivel ${i + 1}, digite um numero de 0 a 100.`);
+            return false;
+        }
+        if(!url.includes('https://')){
+            alert(`Formato de imagem inválido em Nivel ${i + 1}, digite imagens em URL`);
+            return false;
+        }
+        if(descricao.length < 30){
+            alert(`A descrição precisa ter pelo menos 30 caractéres em Nivel ${i + 1}.`);
+            return false;
+        }
+        if(Number(porcentagem) === 0) {
+            contador ++;
+        }
+    }
+    if(contador !== 1) {
+        alert('Uma das porcentagens precisa ser 0')
         return false;
     }
-    if(!url.includes('https://')){
-        alert('Formato de imagem inválido, digite imagens em URL');
-        return false;
-    }
-    if(porcentagem >= 0 && porcentagem <=100){
-        alert('Número inválido, digite um numero de 0 a 100.');
-        return false;
-    }
-    if(descricao.length < 30){
-        alert('O número mínimo de perguntas é 30.');
-        return false;
-    }
+    
+    listarNiveis();
+    renderizarpaginaTresQuatro();
 }
-    return true;
+function listarNiveis () {
+
+    let menorporcentagem = 101;
+
+    for(i = 0; i < numNiveis; i++) {
+       let niveis = document.querySelector(`.nivel${i}`);
+       const titulo = niveis.querySelector(".titulo-nivel").value;  
+       const url = niveis.querySelector(".url-nivel").value;
+       const porcentagem = niveis.querySelector(".pctg-nivel").value;
+       const descricao = niveis.querySelector(".descricao-nivel").value;
+
+       objetoPost.levels[i].title = titulo;
+       objetoPost.levels[i].image = url;
+       objetoPost.levels[i].text = descricao
+       objetoPost.levels[i].minValue = Number(porcentagem);
+    }
+    console.log(objetoPost);
 }
-function renderizarpaginatresquatro() {
+function renderizarpaginaTresQuatro() {
     document.querySelector('.conteudo').innerHTML = `<p>alo</p>`
 }
 function selecionarPergunta(elemento){
     const pai = elemento.parentNode;
     pai.querySelector('.campo-corpo').classList.toggle('escondido')
+}
+function avancaNiveis() {
+    renderizarPaginaTresTres()
 }
